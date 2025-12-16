5f3cb783733b9387dded8df8ef8c0be171b201e0
Syed-Ashfiya9 <syedashfiya4@gmail.com>
2025-12-16 17:31:15 +0530

    Added navbar,content,footer,mainpage components

---
+++ Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f1f1f1",
+        padding: "10px",
+        textAlign: "center",
+        fontSize: "14px",
+        color: "#555",
+      }}
+    >
+      <p>This is Footer</p>
+    </div>
+  );
+}
+
+export default Footer;
